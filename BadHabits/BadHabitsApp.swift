0206ac0fe53b0022d52d7604e25ccbd9f942f571
Vadim <vad.dz2015@yandex.ru>
2026-01-19 16:33:10 +0400

    Initial commit: BadHabits iOS app

---
+++ BadHabits/BadHabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadHabitsApp.swift
+//  BadHabits
+//
+//  Created by Вадим Дзюба on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BadHabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Timer()
+        }
+    }
+}
